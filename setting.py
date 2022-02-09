8faf81206f38ea43f774027804f979e02b724197
whoami <zeroc2537@gmail.com>
2022-02-09 19:14:57 +0800

    最多失败1次

--- setting.py
+++ setting.py
@@ -60,10 +60,10 @@ PROXY_FETCHER = [
 
 # ############# proxy validator #################
 # 代理验证目标网站
-HTTP_URL = "http://www.baidu.com"
+HTTP_URL = "http://httpbin.org"
 HTTPS_URL = "https://www.qq.com"
 VALIDATE_HEADER = 'Server'  # 仅用于HEAD验证方式，百度响应头Server字段KEYWORD可填：bfe   httpbin相应为 gunicorn
-VALIDATE_KEYWORD = 'bfe'
+VALIDATE_KEYWORD = 'gunicorn'
 
 # check 时的线程数量
 CHECK_THREAD = 1000
